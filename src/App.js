32b1072575ee94d36535a127961c0ad33254aecc
omerfaruksaribal <omerfaruk_saribal@hotmail.com>
2024-08-15 14:14:55 +0300

    If page does not exists, then it routes to the home page

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<Home />} />
         </Route>
       </Routes>
     </>
